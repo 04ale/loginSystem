8ddf534ecd0efaaa14abc0ff357135acdb5dce35
04ale <ale.pereira0422@gmail.com>
2025-08-14 09:37:29 -0300

    commit 2

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import Back from "../components/Back";
 import { Link } from "react-router-dom";
 import Logout from "../components/Logout";
+import { useAuth } from "../Authentication";
+
 
 function Profile() {
+
+  const {currentUser}= useAuth();
+
   return (
     <div className="flex flex-col gap-4 justify-center items-center">
       <div className="flex flex-col border-1 py-4 mt-3 bg-white rounded-lg shadow-lg border-gray-300 w-29/30 justify-center items-center gap-3">
@@ -11,6 +16,8 @@ function Profile() {
           <Back />
         </div>
         <h1 className="font-bold text-lg">Profile</h1>
+        <p>Email: {currentUser.email}</p>
+
         <Link
           to="/dashboard"
           className="rounded-md px-3 py-2 bg-blue-400 text-white cursor-pointer"
